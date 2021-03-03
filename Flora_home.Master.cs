121f7253522d844fc559151039618e637637078e
itearthbd <roufmim@gmail.com>
2021-03-03 12:11:07 +0600

    flora

---
+++ Flora_home.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FloraBank.Web
+{
+    public partial class Flora_home : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
